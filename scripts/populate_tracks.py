bb21e076656fa13e7233175edead36d2ee14e1d0
Jacob Pierce <Jake@Jacobs-MacBook-Pro.local>
2015-12-15 14:32:35 -0700

    updated the tracks import

--- scripts/populate_tracks.py
+++ scripts/populate_tracks.py
@@ -19,7 +19,8 @@ for data in response_dict['dataset']:
     #new_tracks.track_title = data['track_title']
     new_tracks.track_url = data['track_url']
     #new_tracks.track_image_field = data['track_image_field']
-    new_tracks.artist_id = int(data['artist_id'])
+    if artist_id != None:
+        new_tracks.artist_id = int(data['artist_id'])
     new_tracks.artist_name = data['artist_name']
     new_tracks.artist_url = data['artist_url']
     new_tracks.license_url = data['license_url']
